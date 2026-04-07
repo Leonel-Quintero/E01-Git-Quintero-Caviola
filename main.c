f09da4522dd124e19cddc5698f0874a9024bcee5
marianocaviola <caviolamariano@gmail.com>
2026-04-07 16:19:45 -0300

    Se creo rama Dev-3

--- main.c
+++ main.c
@@ -7,3 +7,14 @@ void iguales();
 int main() {
 
 }
+void primero() {
+    printf("El primer número ingresado es el mayor.\n");
+}
+
+void segundo() {
+    printf("El segundo número ingresado es el mayor.\n");
+}
+
+void iguales() {
+    printf("Ambos números son iguales.\n");
+}
