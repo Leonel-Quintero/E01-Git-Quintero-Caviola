6fe79155f98968536fe159a64562d1ac52d6276f
Leonel Quintero <leonelbenjaminquintero@gmail.com>
2026-04-07 16:16:52 -0300

    se creo rama Dev-2

--- main.c
+++ main.c
@@ -5,5 +5,14 @@ void segundo();
 void iguales();
 
 int main() {
+    int num1, num2;
+if (num1 > num2) {
+        primero();
+    } else if (num2 > num1) {
+        segundo();
+    } else {
+        iguales();
+    }
 
+    return 0;
 }
